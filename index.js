63bcc13efc469758b17115e570409a96ba1fa462
Sam Verschueren <sam.verschueren@gmail.com>
2015-07-29 08:56:28 +0200

    Exposed the methods

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 // Export DynamoDB
 module.exports = require('./lib/DynamoDB.js');
+
+// Export the methods. Should only be used for testing purposes
+module.exports.methods = {
+    Query: require('./lib/methods/Query'),
+    Scan: require('./lib/methods/Scan'),
+    UpdateItem: require('./lib/methods/UpdateItem'),
+    DeleteItem: require('./lib/methods/DeleteItem')
+};

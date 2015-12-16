743ce7368ec2d3ed6aaae7899a80b67d84349999
Sam Verschueren <sam.verschueren@gmail.com>
2015-12-16 12:01:40 +0100

    add where statement to update item - fixes #34

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ module.exports.Table = require('./lib/Table');
 module.exports.methods = {
 	Query: require('./lib/methods/Query'),
 	Scan: require('./lib/methods/Scan'),
+	InsertItem: require('./lib/methods/InsertItem'),
 	UpdateItem: require('./lib/methods/UpdateItem'),
 	DeleteItem: require('./lib/methods/DeleteItem'),
 	DeleteTable: require('./lib/methods/DeleteTable'),

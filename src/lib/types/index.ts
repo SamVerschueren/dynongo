f5b5a753e31eb2945ee9ce964b1cdaffb35d1ef9
Benjamin <benjaminpalko@hotmail.com>
2021-07-22 15:02:59 -0400

    WhereQuery

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,6 @@
 export { Map } from './map';
 export { Params } from './params';
+export { WhereQuery } from './where-query';
 export { QueryBuilder } from './query-builder';
 export { Schema } from './schema';
 export { UpdateQuery } from './update-query';
---
+++ src/lib/types/where-query.ts
@@ -0,0 +1,41 @@
+interface BaseComparisonQueryOperators<T> {
+	$eq?: T;
+}
+
+interface ComparisonQueryOperators<T> extends BaseComparisonQueryOperators<T> {
+	$in?: T[];
+	$nin?: T[];
+}
+
+interface NumberComparisonOperators extends ComparisonQueryOperators<number> {
+	$gt?: number;
+	$gte?: number;
+	$lt?: number;
+	$lte?: number;
+	$between: [number, number];
+}
+
+interface StringComparisonOperators {
+	$beginsWith: string;
+}
+
+interface ArrayComparisonOperators<T> {
+	$contains: T;
+}
+
+interface LogicalQueryOperators<T> {
+	$and?: WhereQuery<T>[];
+	$not?: WhereQuery<T>;
+	$or?: WhereQuery<T>[];
+}
+interface ElementQueryOperators {
+	$exists?: boolean;
+}
+
+export type WhereQuery<T> = {
+		[Property in keyof T]?: T[Property] extends number
+		? NumberComparisonOperators | T[Property] : T[Property] extends string
+			? StringComparisonOperators | T[Property] : T[Property] extends (infer U)[]
+				? ArrayComparisonOperators<U> | BaseComparisonQueryOperators<T> | ElementQueryOperators | T[Property] : BaseComparisonQueryOperators<T[Property]> | ElementQueryOperators | T[Property]
+	}
+	& LogicalQueryOperators<T>;
